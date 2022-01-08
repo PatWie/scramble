57942be9dc0b471228341cab797e975e5289010a
PatWie <mail@patwie.com>
2022-01-08 13:47:11 +0100

    Refactor implementation

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# scramble
+# Go implementation of Pseudo encrypt
+
+Implements the postgresql [pseudo encrypt](https://wiki.postgresql.org/wiki/Pseudo_encrypt).
+
+```
+Scramble32(x)
+
+ x  | pseudo_encrypt
+-----+----------------
+ -10 |    -1270576520
+  -9 |     -236348969
+  -8 |    -1184061109
+  -7 |      -25446276
+  -6 |    -1507538963
+  -5 |     -518858927
+  -4 |    -1458116927
+  -3 |     -532482573
+  -2 |     -157973154
+  -1 |    -1105881908
+   0 |     1777613459
+   1 |      561465857
+   2 |      436885871
+   3 |      576481439
+   4 |      483424269
+   5 |     1905133426
+   6 |      971249312
+   7 |     1926833684
+   8 |      735327624
+   9 |     1731020007
+  10 |      792482838
+ ```
+
+The functions `Scramble32, Scramble64` are bijective functions, ie. they will shuffle all integers from int32 (resp. int64) with zero collisions.
--- scramble.go
+++ scramble.go
@@ -4,48 +4,86 @@ import (
 	"math"
 )
 
-type Replacer32 func(int32) float64
-type Replacer64 func(int64) float64
+// TODO(patwie): A perfect candidate for generics?
+type (
+	LinearCongurenceParameters32 struct {
+		a     float64
+		c     int32
+		mod   int32
+		scale float64
+	}
 
-func DefaultReplace32(r1 int32) float64 {
-	return float64(((1366.0*r1 + 150889) % 714025)) / 714025.0 * 32767
+	LinearCongurenceParameters64 struct {
+		a     float64
+		c     int64
+		mod   int64
+		scale float64
+	}
+
+	LinearCongurencer32 interface {
+		Transform(int32) float64
+	}
+
+	LinearCongurencer64 interface {
+		Transform(int64) float64
+	}
+)
+
+var DefaultReplace32 = LinearCongurenceParameters32{
+	a:     1366.0,
+	c:     150889,
+	mod:   714025,
+	scale: 32767,
 }
 
-func DefaultReplace64(r1 int64) float64 {
-	return float64(((1366.0*r1 + 150889) % 714025)) / 714025.0 * 32767.0 * 32767.0
+func (r LinearCongurenceParameters32) Transform(v int32) float64 {
+	unbounded := (int32(r.a*float64(v)) + r.c) % r.mod
+	return float64(unbounded) / float64(r.mod) * r.scale
 }
 
-func ScrambleWithFunction32(id int32, replacer Replacer32) int32 {
+var DefaultReplace64 = LinearCongurenceParameters64{
+	a:     1366.0,
+	c:     150889,
+	mod:   714025,
+	scale: 32767 * 32767,
+}
+
+func (r LinearCongurenceParameters64) Transform(v int64) float64 {
+	unbounded := (int64(r.a*float64(v)) + r.c) % r.mod
+	return float64(unbounded) / float64(r.mod) * r.scale
+}
+
+func FeistelNetwork32(start int32, f LinearCongurencer32) int32 {
 	var l1, l2, r1, r2 int32
 
-	l1 = (id >> 16) & 65535
-	r1 = id & 65535
+	l1 = (start >> 16) & 65535
+	r1 = start & 65535
 	for i := 0; i < 3; i++ {
 		l2 = r1
-		r2 = l1 ^ int32(math.Round(replacer(r1)))
+		r2 = l1 ^ int32(math.Round(f.Transform(r1)))
 		l1 = l2
 		r1 = r2
 	}
 	return ((r1 << 16) + l1)
 }
 
-func Scramble32(id int32) int32 {
-	return ScrambleWithFunction32(id, DefaultReplace32)
+func Scramble32(start int32) int32 {
+	return FeistelNetwork32(start, DefaultReplace32)
 }
 
-func ScrambleWithFunction64(id int64, replacer Replacer64) int64 {
+func FeistelNetwork64(start int64, f LinearCongurencer64) int64 {
 	var l1, l2, r1, r2 int64
-	l1 = (id >> 32) & int64(4294967295)
-	r1 = id & int64(4294967295)
+	l1 = (start >> 32) & int64(4294967295)
+	r1 = start & int64(4294967295)
 	for i := 0; i < 3; i++ {
 		l2 = r1
-		r2 = l1 ^ int64(math.Round(replacer(r1)))
+		r2 = l1 ^ int64(math.Round(f.Transform(r1)))
 		l1 = l2
 		r1 = r2
 	}
 	return ((r1 << 32) + l1)
 }
 
-func Scramble64(id int64) int64 {
-	return ScrambleWithFunction64(id, DefaultReplace64)
+func Scramble64(start int64) int64 {
+	return FeistelNetwork64(start, DefaultReplace64)
 }
